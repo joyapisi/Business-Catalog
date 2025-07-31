c1c583d503fee1a99fc505babafb86f1c2557d5b
Joy Phoebe <joyapisi@gmail.com>
2025-07-31 13:30:11 +0300

    Po[ulate images folder

--- src/data/categories.jsx
+++ src/data/categories.jsx
@@ -17,7 +17,7 @@ const categories = [
     image: "/images/led-mirrors/1.jpg",
     path: "led-mirrors",
   },
-    {
+  {
     id: 4,
     title: "Contact Papers",
     image: "/images/contact-papers/1.jpg",
@@ -29,16 +29,16 @@ const categories = [
     image: "/images/wall-stickers/1.jpg",
     path: "kids-stickers",
   },
-    {
+  {
     id: 6,
     title: "Window Films",
     image: "/images/window-films/1.jpg",
     path: "window-films",
   },
-     {
+  {
     id: 7,
     title: "Flexible Marble Sheets",
-    image: "/images/marble-sheet/1.jpg",
+    image: "/images/marble-sheets/1.jpg",
     path: "marble-sheets",
   },
   {
@@ -46,6 +46,30 @@ const categories = [
     title: "Fluted Panels(WPC)",
     image: "/images/wooden-panels/1.jpg",
     path: "fluted-panels",
+  },  
+  {
+    id: 9,
+    title: "PU Stones",
+    image: "/images/pu-stones/1.jpg",
+    path: "pu-stones",
+  },
+  {
+    id: 10,
+    title: "Fly & Mosquito Screens(Anti-Insect screens)",
+    image: "/images/marble-sheet/1.jpg",
+    path: "fly-mosquito screens",
+  },
+  {
+    id: 11,
+    title: "Marble Stickers",
+    image: "/images/marble-stickers/1.jpg",
+    path: "marble-stickers",
+  },
+    {
+    id: 11,
+    title: "Wall to Wall carpets",
+    image: "/images/wall-wall-carpets/1.jpg",
+    path: "wall-wall-carpets",
   },
 ];
 
