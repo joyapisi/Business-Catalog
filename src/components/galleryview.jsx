dacc10e926c64ae98c1ddf8695d42504bff02a7a
Joy Phoebe <joyapisi@gmail.com>
2026-01-14 16:33:08 +0300

    Remove commented code

--- src/components/galleryview.jsx
+++ src/components/galleryview.jsx
@@ -49,30 +49,3 @@ export default function GalleryView({ categoryPath }) {
     </>
   );
 }
-
-
-// import { Grid, Typography } from '@mui/material';
-
-// export default function GalleryView({ category }) {
-//   // Build path name based on category
-//   const folderName = category.toLowerCase().replace(/\s/g, '-');
-//   const imageCount = 500; // Number of images in each category folder
-
-//   const images = Array.from({ length: imageCount }, (_, i) => `/images/${folderName}/${i + 1}.jpg`);
-
-//   return (
-//     <div style={{ padding: '20px' }}>
-//       <Typography variant="h5" align="center" gutterBottom>
-//         {category}
-//       </Typography>
-//       <Grid key={index} sx={{ flexBasis: { xs: '50%', sm: '33.33%', md: '25%' } }}>
-//       <img
-//         src={src}
-//         alt={`img-${index}`}
-//         style={{ width: '100%', height: 'auto', display: 'block', borderRadius: '10px' }}
-//         onError={(e) => (e.target.style.display = 'none')}
-//       />
-//     </Grid>
-//     </div>
-//   );
-// }
