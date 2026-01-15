a84246513482b773b062600432e3b15dda009017
Joy Phoebe <joyapisi@gmail.com>
2026-01-15 14:03:11 +0300

    Fix Netlify Bug Rename Files and restart VS Code to capture Pascal font, which was not pushing to Github hence causing an import error due to a case difference in file naming.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import CategoriesPage from "./pages/CategoriesPage";
-import GalleryPage from "./pages/GalleryPage";
+import GalleryPage from "./pages/galleryPage";
 
 export default function App() {
   return (
