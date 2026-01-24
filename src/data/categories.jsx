d7879ed44e72a2b0594a1764d40d834d69476056
Joy Phoebe <joyapisi@gmail.com>
2026-01-24 15:22:40 +0300

    Add descriptions fro each product

--- src/data/categories.jsx
+++ src/data/categories.jsx
@@ -6,7 +6,7 @@ const categories = [
     path: "wall-murals",
     description: {
       heading: "Customised | Unique | Easy to Clean",
-      body: "KES 2,200 Per Meter Squared (Purchase Only) | KES 3,000 Per Meter Squared (All-Inclusive Package)",
+      body: "Send us a picture and we will design it! \nKES 2,200 Per Meter Squared (Purchase Only) | KES 3,000 Per Meter Squared (All-Inclusive Package) \nOur wall murals are customised according to your wall size \nPro Tip: TOTAL PRICE = Area of your wall(m2) * KES 3,000",
     },
   },
   {
